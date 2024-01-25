e39aca72504fa93253d591fdf55533719e0891e4
SyS <sreyassreelal@gmail.com>
2024-01-25 13:50:06 +0530

    add logo, repo link and stylise header

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,11 +1,27 @@
-import { Typography } from "antd";
+import { Col, Row, Space, Typography } from "antd";
 import { appData } from "../data/app";
 const { Title } = Typography;
+import '../App.css'
+import Link from "antd/es/typography/Link"
 
 const Header = () => {
     return (
         <div style={{ textAlign: "center" }}>
-            <Title>{appData.headertitle}</Title>
+            <Row>
+                <Col span={24}>
+                    <Space>
+                        <Title style={{ fontFamily: "DarumadropOne", fontSize: "3em" }}>{appData.headertitle}</Title>
+                        <img src="logo.png" width={100} height={100} />
+                    </Space>
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+
+                    <Link href={appData.repositorylink}>Github</Link>
+
+                </Col>
+            </Row>
         </div>
     )
 }
