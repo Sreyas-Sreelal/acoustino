93d6f6679aada31284fe7f0611e913161c1141f0
SyS <sreyassreelal@gmail.com>
2024-01-25 13:49:22 +0530

    modify code hilghlighter design

--- src/components/CodeHighlighter.jsx
+++ src/components/CodeHighlighter.jsx
@@ -4,9 +4,10 @@ import { arduinoLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 const CodeHighlighter = ({ code }) => {
     return (
         <SyntaxHighlighter
-            language="c"
+            language="arduino"
             style={arduinoLight}
-            customStyle={{ height: 200, maxHeight: 200, minHeight: 200, overflowY: scroll, width: "100%", padding: 0 }}
+            customStyle={{ height: 230, maxHeight: 230, minHeight: 230, overflowY: scroll, padding: 0,
+            margin: 0 }}
             lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
             wrapLines
         >
