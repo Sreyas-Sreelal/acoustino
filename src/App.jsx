63e124acb72a846ad9cfde13da105dd0d3527814
SyS <sreyassreelal@gmail.com>
2024-01-25 12:10:56 +0530

    initial front end design

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,40 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { open } from "@tauri-apps/api/dialog";
-import { Button, Col, Divider, InputNumber, Row, Select, Space, Typography } from "antd";
-import TextArea from "antd/es/input/TextArea";
-import { FileAddOutlined } from "@ant-design/icons";
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { arduinoLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { Col, Divider, Row } from "antd";
+
+import CodeArea from "./container/CodeArea";
+import Header from "./container/Header";
+import Prepare from "./container/Prepare";
+import { appData } from "./data/app";
 
 function App() {
   const [output, setOutput] = useState("");
   const [pin, setPin] = useState(11);
 
-  function onSelectPin(event) {
-    setPin(parseInt(event.target.value));
-  }
-
-  async function onClickConvert(event) {
+  async function onChooseFile(event) {
     let filepath = await open()
     setOutput(await invoke("convert", { file: filepath, pin }));
   }
 
-
   return (
     <>
-      <Row>
-
+      <Row >
         <Col span={24}>
-          <div style={{ textAlign: "center" }}>
-            <Typography.Title>Acoustino</Typography.Title>
-          </div>
-        </Col>
-      </Row>
-      {/* <Divider /> */}
-      <Row>
-        <Col span={24}>
-          <Typography.Title level={5}>Generated Code</Typography.Title>
+          <Header />
         </Col>
       </Row>
       <Row>
         <Col span={24}>
-          <SyntaxHighlighter
-            language="c"
-            style={arduinoLight}
-            customStyle={{ height: 200, maxHeight: 200, minHeight: 200, overflowY: scroll, width: "100%" }}
-            lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
-            wrapLines  >
-            {output}
-          </SyntaxHighlighter>
+          <CodeArea code={output} />
         </Col>
       </Row>
-      <Divider />
+      <Divider>
+        {appData.optionsectiontitle}
+      </Divider>
       <Row>
         <Col span={24}>
-          <div style={{ textAlign: "center" }}>
-            <Space>
-              <Typography.Text>Pin no: </Typography.Text>
-              <InputNumber min={1} max={11} onChange={setPin} value={pin} />
-              <Button type="primary" icon={<FileAddOutlined />} onClick={onClickConvert}>
-                Choose File to Convert
-              </Button>
-            </Space>
-          </div>
+          <Prepare ChooseFileHandler={onChooseFile} setPin={setPin} pin={pin} />
         </Col>
       </Row>
     </>
