ed794e5ea989b6851ce6ce528d36544495c81975
SyS <sreyassreelal@gmail.com>
2024-01-25 13:45:50 +0530

    repository link added

--- src/data/app.js
+++ src/data/app.js
@@ -1,10 +1,11 @@
 export const appData = {
     "choosefilebuttontext": "Choose File",
     "optionsectiontitle": "Prepare",
-    "headertitle": "Accoustino",
+    "headertitle": "Acoustino",
     "codeareatitle": "Generated Code",
     "pinselectlabel": "Pin Number",
     "copybuttontext": "Copy",
     "arduinominpin": 1,
     "arduinomaxpin": 11,
+    "repositorylink":"https://github.com/sreyas-sreelal/acoustino"
 }
