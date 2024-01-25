40873393cef8e10c2bebd241b75f560aacf45316
SyS <sreyassreelal@gmail.com>
2024-01-25 12:09:10 +0530

    app data

---
+++ src/data/app.js
@@ -0,0 +1,10 @@
+export const appData = {
+    "choosefilebuttontext": "Choose File",
+    "optionsectiontitle": "Prepare",
+    "headertitle": "Accoustino",
+    "codeareatitle": "Generated Code",
+    "pinselectlabel": "Pin Number",
+    "copybuttontext": "Copy",
+    "arduinominpin":1,
+    "arduinomaxpin":11,
+}
