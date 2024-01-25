be1e57b6da70805a564707986093396c0777a2c9
SyS <sreyassreelal@gmail.com>
2024-01-25 12:09:53 +0530

    add custom button components

---
+++ src/components/FileChooseButton.jsx
@@ -0,0 +1,12 @@
+import { FileAddOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
+const FileChooseButton = ({ text,handler }) => {
+    return (
+        <Button type="primary" icon={<FileAddOutlined />} onClick={handler}>
+            {text}
+        </Button>
+    )
+}
+
+export default FileChooseButton;
